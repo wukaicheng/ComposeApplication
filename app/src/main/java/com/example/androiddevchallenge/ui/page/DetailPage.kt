235945508dce6c50f038b734552f9c274cd97303
kai-city <1830170041@qq.com>
2021-03-01 21:51:24 +0800

    1.fix error 2.upload screenshot

--- app/src/main/java/com/example/androiddevchallenge/ui/page/DetailPage.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/page/DetailPage.kt
@@ -17,24 +17,28 @@ package com.example.androiddevchallenge.ui.page
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.text.font.FontWeight
@@ -46,6 +50,10 @@ import com.example.androiddevchallenge.ui.Pager
 import com.example.androiddevchallenge.ui.PagerState
 import com.example.androiddevchallenge.ui.theme.MyTheme
 import com.example.androiddevchallenge.ui.theme.typography
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 @Composable
 fun DetailPage(puppy: Puppy) {
@@ -140,17 +148,6 @@ private fun ProfileItem(key: String, value: String) {
     Text(value, fontWeight = FontWeight.Light)
 }
 
-@Composable
-fun CarouselDot(selected: Boolean, icon: ImageVector) {
-    Icon(
-        imageVector = icon,
-        modifier = Modifier
-            .padding(4.dp)
-            .size(12.dp),
-        contentDescription = null,
-        tint = if (selected) Color.Gray else Color.LightGray
-    )
-}
 
 @Preview
 @Composable
